0686466214b9394b5ae51dca25481d6bf4b086e3
lgoteni <levigoteni@gmail.com>
2022-01-21 17:37:29 +0100

    indentation

--- src/App.js
+++ src/App.js
@@ -3,37 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  state={
-    post:{}
-  }
-  componentDidMount(){
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => { return response.json()})
-
-  .then(result => {
-    this.setState({post:result})})
+  
+  state={ post:{} }
+  
+  componentDidMount()
+  {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => { return response.json()})
+      .then(result => {
+      this.setState({post:result})})
   }
   render(){
     
     return <div>
-    <h1></h1>
-    <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Titre</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">{this.state.post.id}</th>
-      <td>{this.state.post.title}</td>
-    </tr>
-    
-  </tbody>
-</table>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Titre</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">{this.state.post.id}</th>
+            <td>{this.state.post.title}</td>
+          </tr>
+          
+        </tbody>
+      </table>
     </div>
   }
 }
-
 export default App;
